bad39072a14e74ecd9e9a01ad4dbb5238f659f8e
sverx <sverx@yahoo.com>
2016-08-25 13:37:10 +0200

    updated source (that was forgotten)

--- assets2banks/src/assets2banks.py
+++ assets2banks/src/assets2banks.py
@@ -19,12 +19,12 @@ class AssetGroup:
          self.size += asset.size
 
 class Bank:
-     def __init__(self): 
+     def __init__(self,size): 
          self.assetgroups = [] 
-         self.size = 0
+         self.free = size
      def add_assetgroup(self, assetgroup): 
          self.assetgroups.append(assetgroup) 
-         self.size += assetgroup.size
+         self.free -= assetgroup.size
 
 def find(f, seq):
   for item in seq:
@@ -37,24 +37,38 @@ BankList = []          # list of the banks
 
 print ("*** sverx's assets2banks converter ***")
 
-if len(sys.argv)==2:
+if len(sys.argv)==2 or len(sys.argv)==3:
     assets_path=sys.argv[1]
+    first_bank=2                                                # first bank will be number 2
 else:
-    print ("Usage: assets2banks path")
-    exit(1)
+    print ("Usage: assets2banks path [--bank1size=<size>]")
+    sys.exit(1)    
+
+if len(sys.argv)==3:
+    if sys.argv[2][:12]=="--bank1size=":
+        bank1size=sys.argv[2][12:]
+        b = Bank(int(sys.argv[2][12:]))
+        BankList.append(b)
+        first_bank=1                                            # first bank should be number 1
+        print ("Info: bank1 size set at " + str(b.free) + " bytes")
+    else:
+        print ("Usage: assets2banks path [--bank1size=<size>]")
+        sys.exit(1)
 
 in_a_group = False                                              # read cfg file (if present) and create assets and assets group accordingly
 try:
     config_file = open (os.path.join(assets_path, "assets2banks.cfg"),"r").readlines()
     for l in config_file:
-        if l[0] == "#":                                         # if line starts with #, it is a comment, we can skip it
+        if len(l.strip()) == 0:                                 # if line is empty or made of just spaces, skip it
+            pass 
+        elif l[0] == "#":                                       # if line starts with #, it is a comment, we can skip it
             pass
         elif l[0] == "{":                                       # if line starts with {, it means we are beginning a group
             ag = AssetGroup()
             AssetGroupList.append(ag)
-            in_a_group = True;
+            in_a_group = True
         elif l[0] == "}":                                       # if line starts with }, it means we are closing an open group
-            in_a_group = False;
+            in_a_group = False
         elif l[0] == ":":                                       # if line starts with :, it means we have an attribute
             if l.strip('\n')==":format unsigned int":
               a.set_style(1)
@@ -95,22 +109,26 @@ for f in os.listdir(assets_path):                          # read directory cont
 AssetGroupList.sort(key=lambda g: g.size, reverse=True)    # sort the AssetGroupList by size, descending
 
 for ag in AssetGroupList:                                  # now find a place for each assetgroup
-#    print (ag.assets, ag.size)
+#    print ("debug: assetgroup size ", ag.size)
     for b in BankList:
-#        print (b.size) 
-        if (b.size+ag.size)<=16384:                        # we've found a place for this assetgroup
-#            print ("added to existing bank") 
+#        print ("debug: bank has ", b.free, " bytes free") 
+        if b.free>=ag.size:                                # we've found a place for this assetgroup
+#            print ("debug: added to existing bank") 
             b.add_assetgroup(ag)
             break;                       
     else:                                                  # we've found no place for this assetgroup, allocate a new bank
-        b = Bank()
-        b.add_assetgroup(ag)
-        BankList.append(b)
-#        print ("added to new bank") 
+        if ag.size<=16384:
+            b = Bank(16384)
+            b.add_assetgroup(ag)
+            BankList.append(b)
+#            print ("debug: added to new bank")
+        else:
+            print ("Fatal: asset/assetgroup too big to fit - " + str(ag.size) + " bytes are needed.")
+            sys.exit(1)
 
 for bank_n, b in enumerate (BankList):
-    out_file_h = open ("bank" + str(bank_n + 2) + ".h","w")
-    out_file_c = open ("bank" + str(bank_n + 2) + ".c","w")
+    out_file_h = open ("bank" + str(bank_n + first_bank) + ".h","w")
+    out_file_c = open ("bank" + str(bank_n + first_bank) + ".c","w")
     for ag in b.assetgroups:
         for a in ag.assets:
             in_file = open (os.path.join(assets_path, a.name),"rb")
@@ -122,7 +140,7 @@ for bank_n, b in enumerate (BankList):
                 out_file_h.write("extern const unsigned int\t" + a.fixed_name() + "[" + str(a.size//2) + "];\n")
                  
             out_file_h.write("#define\t\t\t\t" + a.fixed_name() + "_size " + str(a.size) + "\n")
-            out_file_h.write("#define\t\t\t\t" + a.fixed_name() + "_bank " + str(bank_n + 2) + "\n")
+            out_file_h.write("#define\t\t\t\t" + a.fixed_name() + "_bank " + str(bank_n + first_bank) + "\n")
 
             if a.style==0:
                 cnt = 0
@@ -155,7 +173,10 @@ for bank_n, b in enumerate (BankList):
     out_file_c.close()
 else:
     if len(BankList)==0:
-        print ("Fatal: no banks generated\n")
-        exit(1)
+        print ("Fatal: no banks generated")
+        sys.exit(1)
     else:
-        print ("Info: " + str(bank_n + 1) + " banks generated\n")
+        print ("Info: " + str(len(BankList)) + " bank(s) generated", end="")
+        for bank_n, b in enumerate (BankList):
+            print (" [bank" + str(bank_n + first_bank) + ":" + str(b.free) + "]", end=""),
+        print (" bytes free")
