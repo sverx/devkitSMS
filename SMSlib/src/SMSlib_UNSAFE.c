f16d91ca569cbd1e20ba5d9c56ed2e6d489c85b2
sverx <sverx@yahoo.com>
2017-12-07 17:14:55 +0100

    slightly better UNSAFE_SMS_copySpritestoSAT
    (no real need to upgrade)

--- SMSlib/src/SMSlib_UNSAFE.c
+++ SMSlib/src/SMSlib_UNSAFE.c
@@ -21,6 +21,53 @@ void UNSAFE_SMS_copySpritestoSAT (void) {
     ld a,(#_SpriteNextFree)
     or a
     jr z,_no_sprites
+    add a,a                 ; SpriteNextFree*=2 (and reset carry)
+    ld c,a
+    ld b,#0
+    ld hl,#_outi_block
+    sbc hl,bc
+    ex de,hl
+    ld hl,#_SpriteTableY
+    call _do_copy_Y
+    ld a,(#_SpriteNextFree)
+    cp #64
+    jr z,_no_sprite_term
+    ld a,#0xD0
+    out (c),a
+_no_sprite_term:
+  __endasm;
+ SMS_setAddr(SMS_SATAddress+128);
+  __asm
+    ld a,(#_SpriteNextFree)
+    dec a                   ; there is surely at least one sprite used
+    add a,a
+    add a,a                 ; a=(SpriteNextFree-1)*4 (and reset carry)
+    ld c,a
+    ld b,#0
+    ld hl,#_outi_block-4
+    sbc hl,bc
+    push hl                 ; push jump address into stack
+    ld hl,#_SpriteTableXN
+    ld c,#_VDPDataPort
+    ret                     ; get jump address from stack
+_do_copy_Y:
+    ld c,#_VDPDataPort
+    push de
+    ret                     ; get jump address from stack
+_no_sprites:
+    ld a,#0xD0
+    out (#_VDPDataPort),a
+  __endasm;
+}
+
+/*
+// previous version
+void UNSAFE_SMS_copySpritestoSAT (void) {
+  SMS_setAddr(SMS_SATAddress);
+  __asm
+    ld a,(#_SpriteNextFree)
+    or a
+    jr z,_no_sprites
     ld b,a
     ld a,#64
     sub a,b
@@ -55,35 +102,11 @@ _no_sprite_term:
     ld hl,#_SpriteTableXN
 _start_cpy:
     ld c,#_VDPDataPort
-    push de
-    pop iy
-    jp (iy)
+    push de                ; same as jp (de)
+    ret                    ; which does not exists
 _no_sprites:
     ld a,#0xD0
     out (#_VDPDataPort),a
-  __endasm;
-}
-
-/*
-// previous version - profiling: 3283 cycles (14 scanlines)
-void UNSAFE_SMS_copySpritestoSAT (void) {
-  // SMS_set_address_VRAM(SMS_SATAddress);
-  SMS_setAddr(SMS_SATAddress);
-  __asm
-    ld c,#_VDPDataPort
-    ld hl,#_SpriteTableY
-#if MAXSPRITES==64
-    call _outi_block-MAXSPRITES*2
-#else
-    call _outi_block-(MAXSPRITES+1)*2
-#endif
-  __endasm;
-  // SMS_set_address_VRAM(SMS_SATAddress+128);
-  SMS_setAddr(SMS_SATAddress+128);
-  __asm
-    ld c,#_VDPDataPort
-    ld hl,#_SpriteTableXN
-    call _outi_block-MAXSPRITES*4
   __endasm;
 }
 */
@@ -95,21 +118,18 @@ void OUTI128(void *src) __z88dk_fastcall;
 #define SETVDPDATAPORT  __asm ld c,#_VDPDataPort __endasm
 
 void UNSAFE_SMS_VRAMmemcpy32 (unsigned int dst, void *src) {
-  // SMS_set_address_VRAM(dst);
   SMS_setAddr(0x4000|dst);
   SETVDPDATAPORT;
   OUTI32(src);
 }
 
 void UNSAFE_SMS_VRAMmemcpy64 (unsigned int dst, void *src) {
-  // SMS_set_address_VRAM(dst);
   SMS_setAddr(0x4000|dst);
   SETVDPDATAPORT;
   OUTI64(src);
 }
 
 void UNSAFE_SMS_VRAMmemcpy128 (unsigned int dst, void *src) {
-  // SMS_set_address_VRAM(dst);
   SMS_setAddr(0x4000|dst);
   SETVDPDATAPORT;
   OUTI128(src);
