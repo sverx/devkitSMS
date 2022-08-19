5797c7e22087fe773fc8a16a19ea80df2f43e63a
Raphael Assenat <raph@raphnet.net>
2022-08-19 21:33:50 +0900

    Fix an implementation error in the previous commit
    + style correction (no tabs, two space indentation)

--- SMSlib/src/SMSlib_loadTileMapArea.c
+++ SMSlib/src/SMSlib_loadTileMapArea.c
@@ -9,20 +9,18 @@
 void SMS_loadTileMapArea (unsigned char x, unsigned char y, const void *src, unsigned char width, unsigned char height) {
   unsigned char sz = width * 2;
   const char *data = (const char *)src;
-  unsigned char skip = 64 - width * 2;
   unsigned int vdp_addr;
 
   vdp_addr = SMS_PNTAddress+(y*32+x)*2;
 
-  y = height;
   do {
     SMS_setAddr(vdp_addr);
-	x = sz;
-	do {
-	    VDPDataPort=*(data++);
-	} while(--x);
 
-	data += skip;
-	vdp_addr += 64;
-  } while(--y);
+    x = sz;
+    do {
+      VDPDataPort=*(data++);
+    } while(--x);
+
+    vdp_addr += 64;
+  } while(--height);
 }
