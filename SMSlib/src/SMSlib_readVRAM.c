b877f01f52c85940892440dbe7983b8dec57e779
Raphaël Assenat <raph@raphnet.net>
2022-12-08 22:29:46 +0900

    Add basic read from VRAM functionality (#47)
    * Add basic read from VRAM functionality

---
+++ SMSlib/src/SMSlib_readVRAM.c
@@ -0,0 +1,38 @@
+/* **************************************************
+   SMSlib - C programming library for the SMS/GG
+   ( part of devkitSMS - github.com/sverx/devkitSMS )
+   ************************************************** */
+#include "SMSlib.h"
+
+void SMS_saveTileMapArea(unsigned char x, unsigned char y, void *dst, unsigned char width, unsigned char height)
+{
+    unsigned char i,j;
+    unsigned short *d = dst;
+
+    for (i=0; i<height; i++) {
+        SMS_setAddr(SMS_PNTAddress_r + (y + i) * 64 + x * 2);
+        for (j=0; j<width; j++) {
+            *d++ = SMS_getTile();
+        }
+    }
+}
+
+#pragma save
+#pragma disable_warning 85
+
+unsigned short SMS_getTile(void) __z88dk_fastcall __naked
+{
+    __asm
+
+    in a, (#0xBE)  ; 11
+    ld l, a        ; 4
+    inc hl         ; 6
+    dec hl         ; 6
+    in a, (#0xBE)  ; 11
+    ld h, a        ; 4
+    ret
+
+    __endasm;
+}
+
+#pragma restore
