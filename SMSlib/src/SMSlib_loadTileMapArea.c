76914212dca42cffb522c4553dd28f0f5ca933a9
sverx <sverx@yahoo.com>
2023-11-14 16:22:07 +0100

    faster code for SMS_loadTileMapArea
    inner loop now 7 cycles faster, outer loop improved too

--- SMSlib/src/SMSlib_loadTileMapArea.c
+++ SMSlib/src/SMSlib_loadTileMapArea.c
@@ -17,21 +17,13 @@ void SMS_loadTileMapAreaatAddr (unsigned int dst, const void *src, unsigned char
     pop iy             ; pop height (iyh) and width (iyl)
     push bc            ; push ret address
 
-    push ix            ; we need to preserve ix
-
-                       ; now move height in ixl
-    .db 0xFD           ;   --- SDCC issues workaround
-    ld a,h             ; ld a,iyh
-    .db 0xDD           ;   --- SDCC issues workaround
-    ld l,a             ; ld ixl,a
-
-    .db 0xFD           ;   --- SDCC issues workaround
-    ld h,l             ; ld iyh,iyl  ; preserve width in iyh
-
     set 6,h            ; set VRAM address for write
-    ld bc,#64          ; preload VRAM offset between lines
+    ld c,#64           ; preload VRAM offset between lines
 
 1$:
+    .db 0xFD           ;   --- SDCC issues workaround
+    ld b,l             ; ld b,iyl ; load width into B
+
     ld a,l
     di                 ; make it interrupt SAFE
     out (#_VDPControlPort),a
@@ -40,30 +32,20 @@ void SMS_loadTileMapAreaatAddr (unsigned int dst, const void *src, unsigned char
     ei
 
 2$:
-    ld a,(de)                                                     ; 7
-    out (#_VDPDataPort),a
-    inc de                    ; 6
-    ld a,(de)                 ; 7
-    inc de                    ; 6
-    out (#_VDPDataPort),a     ; 11
+    ld a,(de)                   ; 7
+    out (#_VDPDataPort),a       ; 11 = 31
+    inc de                                  ; 6
+    ld a,(de)                               ; 7
+    inc de                                  ; 6
+    out (#_VDPDataPort),a                   ; 11 = 30
+    djnz 2$                     ; 13
 
     .db 0xFD           ;   --- SDCC issues workaround
-    dec l              ; dec iyl ; decrement the width counter    ; 8
-    jr nz,2$           ; loop over until zero                     ; 12
+    dec h              ; dec iyh ; decrement the height counter
+    ret z              ; leave if zero
 
-    .db 0xDD           ;   --- SDCC issues workaround
-    dec l              ; dec ixl ; decrement the height counter
-    jr z,3$            ; leave if zero
-
-    add hl,bc          ; add offset to dest address
-
-    .db 0xFD           ;   --- SDCC issues workaround
-    ld l,h             ; ld iyl,iyh  ; reload width
+    add hl,bc          ; add offset to dest address (B is zero, C is 64)
     jp 1$
-
-3$:
-    pop ix             ; restore original ix
-    ret
   __endasm;
 }
 #pragma restore
