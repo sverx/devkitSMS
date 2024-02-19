37b6ee853cd3308b95659b204c7a38a4240d9f1a
sverx <sverx@yahoo.com>
2024-02-19 10:45:43 +0100

    faster SMS_loadTileMapArea (and SMS_loadTileMapAreaatAddr)

--- SMSlib/src/SMSlib_loadTileMapArea.c
+++ SMSlib/src/SMSlib_loadTileMapArea.c
@@ -8,6 +8,7 @@
 
 #pragma save
 #pragma disable_warning 85
+// now even faster!
 void SMS_loadTileMapAreaatAddr (unsigned int dst, const void *src, unsigned char width, unsigned char height) __naked __z88dk_callee __sdcccall(1) {
   // dst in hl
   // src in de
@@ -18,32 +19,33 @@ void SMS_loadTileMapAreaatAddr (unsigned int dst, const void *src, unsigned char
     push bc            ; push ret address
 
     set 6,h            ; set VRAM address for write
-    ld c,#64           ; preload VRAM offset between lines
+    .db 0xFD           ;   --- SDCC issues workaround
+    ld a,l             ; ld a,iyl ; load width into A
 
 1$:
-    .db 0xFD           ;   --- SDCC issues workaround
-    ld b,l             ; ld b,iyl ; load width into B
-
-    ld a,l
+    ld c,#_VDPControlPort
     di                 ; make it interrupt SAFE
-    out (#_VDPControlPort),a
-    ld a,h
-    out (#_VDPControlPort),a
+    out (c),l
+    out (c),h
     ei
 
+    dec c              ; faster than ld c,#_VDPDataPort
+    ex de,hl
+    ld b,a             ; B=width
+
 2$:
-    ld a,(de)                   ; 7
-    out (#_VDPDataPort),a       ; 11 = 31
-    inc de                                  ; 6
-    ld a,(de)                               ; 7
-    inc de                                  ; 6
-    out (#_VDPDataPort),a                   ; 11 = 30
-    djnz 2$                     ; 13
+    outi                  ; 16
+    inc b                 ;  4
+    sub #0                ;  7 = 27 : SAFE on every SMS and GG
+    outi         ; 16
+    jr nz,2$     ; 12 = 28 : SAFE on every SMS and GG
 
     .db 0xFD           ;   --- SDCC issues workaround
     dec h              ; dec iyh ; decrement the height counter
     ret z              ; leave if zero
 
+    ex de,hl
+    ld c,#64
     add hl,bc          ; add offset to dest address (B is zero, C is 64)
     jp 1$
   __endasm;
