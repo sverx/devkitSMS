c8248a8b61e0debf2c8df3dc178e7a24472adf66
sverx <sverx@yahoo.com>
2024-12-13 17:53:23 +0100

    (bugfix)
    damn

--- SGlib/src/SGlib.h
+++ SGlib/src/SGlib.h
@@ -149,8 +149,8 @@ unsigned int SG_getKeyboardJoypadReleased (void);
 unsigned char SG_getKeycodes (unsigned int *keys, unsigned char max_keys);
 
 /* functions to decompress data to VRAM */
-#define SG_loadZX7compressedBGTiles(src,tilefrom)      SG_decompressZX7toVRAM((src),PGTADDRESS+((tilefrom)<<3))
-#define SG_loadZX7compressedSpritesTiles(src,tilefrom) SG_decompressZX7toVRAM((src),SGTADDRESS+((tilefrom)<<3))
+#define SG_loadZX7compressedBGTiles(src,tilefrom)      SG_decompressZX7toVRAM((src),0x4000+((tilefrom)<<3))
+#define SG_loadZX7compressedSpritesTiles(src,tilefrom) SG_decompressZX7toVRAM((src),0x7800+((tilefrom)<<3))
 #define SG_loadZX7compressedTilesatAddr(src,dst)       SG_decompressZX7toVRAM((src),(dst))
 void SG_decompressZX7toVRAM (const void *src, unsigned int dst) __naked;
 
