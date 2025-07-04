881a0fac6c024b74e2f273e48b6e26f99a06a93c
sverx <sverx@yahoo.com>
2025-07-04 17:03:36 +0200

    add GG_loadBGPaletteHalfBrightness / GG_loadSpritePaletteHalfBrightness
    (the GG counterparts of the SMS_load*PaletteHalfBrightness functions)

--- SMSlib/src/SMSlib_paletteAdv.c
+++ SMSlib/src/SMSlib_paletteAdv.c
@@ -35,7 +35,37 @@ void SMS_loadSpritePaletteHalfBrightness (const void *palette) __z88dk_fastcall 
     and #0x15                  ; 7
     out (_VDPDataPort),a       ; 11
     inc hl                     ; 6
-    djnz 1$                    ; 13  *VRAM SAFE*
+    djnz 1$                    ; 13 = *VRAM SAFE*
+  __endasm;
+}
+#else
+void GG_loadBGPaletteHalfBrightness (const void *palette) __z88dk_fastcall {
+  // *palette will be in HL
+  ASM_LD_DE_IMM(#SMS_CRAMAddress);
+  ASM_DE_TO_VDP_CONTROL;
+  ASM_LD_B_IMM(#32);
+  __asm
+1$: ld a,(hl)                  ; 7
+    rrca                       ; 4
+    and #0x77                  ; 7
+    out (_VDPDataPort),a       ; 11
+    inc hl                     ; 6
+    djnz 1$                    ; 13 = *VRAM SAFE*
+  __endasm;
+}
+
+void GG_loadSpritePaletteHalfBrightness (const void *palette) __z88dk_fastcall {
+  // *palette will be in HL
+  ASM_LD_DE_IMM(#SMS_CRAMAddress+0x20);
+  ASM_DE_TO_VDP_CONTROL;
+  ASM_LD_B_IMM(#32);
+  __asm
+1$: ld a,(hl)                  ; 7
+    rrca                       ; 4
+    and #0x77                  ; 7
+    out (_VDPDataPort),a       ; 11
+    inc hl                     ; 6
+    djnz 1$                    ; 13 = *VRAM SAFE*
   __endasm;
 }
 #endif
